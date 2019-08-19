ff446bcb16088b045a2709cc311bec7df8dbb3a4
HermesBoots <701@holbertonschool.com>
2019-08-19 18:00:07 +0000

    Make task 4 also clean local files

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,6 +3,7 @@
 
 
 import fabric.api
+import os
 
 
 fabric.api.env.hosts = ['35.229.22.85', '34.74.166.73']
@@ -12,13 +13,16 @@ fabric.api.env.user = 'ubuntu'
 def do_clean(number=0):
     """Clean outdated releases from web servers"""
 
-    command = 'ls -1 --directory '
-    command += '/data/web_static/releases/web_static_* '
-    command += '| sort --numeric-sort --reverse'
-    releases = fabric.api.run(command).splitlines()
     number = int(number)
     if number < 1:
         number = 1
+    releases = sorted(os.listdir('versions'))[:-number]
     releases = releases[number:]
+    for archive in releases:
+        fabric.api.local('rm versions/' + archive)
+    command = 'ls -1 --directory '
+    command += '/data/web_static/releases/web_static_* '
+    command += '| sort --numeric-sort --reverse'
+    releases = fabric.api.run(command).splitlines()[number:]
     for directory in releases:
         fabric.api.run('rm -rf /data/web_static/' + directory)
