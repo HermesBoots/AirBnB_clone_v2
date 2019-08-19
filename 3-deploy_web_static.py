7c3f74bbed622748202dd3a6cd164b0cf89cea53
HermesBoots <701@holbertonschool.com>
2019-08-19 17:07:26 +0000

    Add task 3: script that combines 2 previous tasks

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module to make tarball from a local directory"""
+
+
+import datetime
+import fabric.api
+import os
+import os.path
+
+
+fabric.api.env.hosts = ['35.229.22.85', '34.74.166.73']
+fabric.api.env.user = 'ubuntu'
+
+
+def deploy():
+    """Fully deploy the web stack"""
+
+    archive = do_pack()
+    if archive is None:
+        return False
+    return do_deploy(archive)
+
+
+def do_deploy(archive_path):
+    """Upload a web site archive to a remote web server and install it"""
+
+    if not os.path.exists(archive_path):
+        return False
+    path = fabric.api.put(archive_path, '/tmp/')[0]
+    target = '/data/web_static/releases/' 
+    target += os.path.splitext(os.path.split(path)[1])[0]
+    fabric.api.run('mkdir -p ' + target)
+    fabric.api.run('tar -xzf ' + path + ' -C ' + target)
+    fabric.api.run('rm ' + path)
+    fabric.api.run('mv ' + target + '/web_static/* ' + target + '/')
+    fabric.api.run('rm -rf ' + target + '/web_static')
+    fabric.api.run('rm -rf /data/web_static/current')
+    fabric.api.run('ln -s ' + target + '/ /data/web_static/current')
+    return True
+
+
+def do_pack():
+    """Bundle and compress the local static web files"""
+
+    if not os.path.isdir('versions'):
+        os.mkdir('versions')
+    target = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    target = 'versions/web_static_' + target + '.tgz'
+    fabric.api.local('tar -cvzf ' + target + ' web_static')
+    return target
