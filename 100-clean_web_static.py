5b871b7450ec937193c7d126cdfc08f24b69669d
HermesBoots <701@holbertonschool.com>
2019-08-19 17:08:41 +0000

    Add task 4: delete out-of-date directories

---
+++ 100-clean_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Clean outdated releases from web servers"""
+
+
+import fabric.api
+
+
+fabric.api.env.hosts = ['35.229.22.85', '34.74.166.73']
+fabric.api.env.user = 'ubuntu'
+
+
+def do_clean(number=0):
+    """Clean outdated releases from web servers"""
+
+    command = 'ls -1 --directory '
+    command += '/data/web_static/releases/web_static_* '
+    command += '| sort --numeric-sort --reverse'
+    releases = fabric.api.run(command).splitlines()
+    number = int(number)
+    if number < 1:
+        number = 1
+    releases = releases[number:]
+    for directory in releases:
+        fabric.api.run('rm -rf /data/web_static/' + directory)
