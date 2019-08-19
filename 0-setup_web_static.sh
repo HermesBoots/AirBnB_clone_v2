25c0975f709bcf6588ec570f9596313f87769412
HermesBoots <701@holbertonschool.com>
2019-08-19 17:04:28 +0000

    Add task 0: set up deployment folders on web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# install directories on remote server for serving web releases
+
+apt-get update
+apt-get install nginx
+mkdir --parents /data/web_static/releases/test
+mkdir --parents /data/web_static/shared
+ln --force --symbolic /data/web_static/releases/test /data/web_static/current
+cat > /data/web_static/releases/test/index.html << EOF
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fabric Test</title>
+</head>
+<body>
+	<p>Nothing for now.</p>
+</body>
+</html>
+EOF
+chown --recursive ubuntu:ubuntu /data
+cat > /etc/nginx/sites-available/default << EOF
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server ipv6only=on;
+	root /var/www/html;
+	index index.html;
+
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+	}
+}
+EOF
+nginx -s reload
