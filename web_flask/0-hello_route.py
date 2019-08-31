67d358d8479f2940dc63f370ce56d29a8c5dfd1a
HermesBoots <701@holbertonschool.com>
2019-08-31 23:33:06 +0000

    Add task 0: most basic Flask server

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for very basic Flask server"""
+
+
+import flask
+
+
+site = flask.Flask(__name__)
+
+
+@site.route('/', strict_slashes=False)
+def index():
+    """Display the site's index"""
+
+    return 'Hello HBNB!\n'
+
+
+site.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,4 @@
+# AirBnB Clone - Web Framework
+
+## Hello Flask!
+Define a very basic web server with an index.
